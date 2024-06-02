5edbe31a4d877a6a26ad26e89dee0075c735f6f8
Mamariia <148610516+Mamariia@users.noreply.github.com>
2024-06-02 18:51:20 +0500

    Add files via upload

---
+++ module_5_2.py
@@ -0,0 +1,14 @@
+class House:
+    def __init__(self):
+        self.numberOfFloors = 0
+
+    def setNewNumberOfFloors(floors):
+        numberOfFloors = floors
+        print(numberOfFloors)
+
+
+h1 = House
+h1.setNewNumberOfFloors(12)
+
+
+
